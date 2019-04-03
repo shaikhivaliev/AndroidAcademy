7e9115ac432a9266051f468ee86f92bf331ea843
shaikhivaliev <shaikhivaliev@gmail.com>
2019-04-03 17:45:51 +0300

    report layout

--- app/src/main/java/com/example/academytask1/MainActivity.java
+++ app/src/main/java/com/example/academytask1/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.academytask1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mAllReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAllReport = findViewById(R.id.btn_all_reports);
+        mAllReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Переход ко всем докладам", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
