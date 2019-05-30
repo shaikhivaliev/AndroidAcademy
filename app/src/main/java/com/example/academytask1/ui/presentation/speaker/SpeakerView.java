da91f52b66ea8ab44f12114036aa5175842dd616
shaikhivaliev <shaikhivaliev@gmail.com>
2019-05-30 12:30:16 +0300

    moxy

--- app/src/main/java/com/example/academytask1/ui/presentation/speaker/SpeakerView.java
+++ app/src/main/java/com/example/academytask1/ui/presentation/speaker/SpeakerView.java
@@ -1,8 +1,12 @@
 package com.example.academytask1.ui.presentation.speaker;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.example.academytask1.ui.entity.Speaker;
 
+
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface SpeakerView extends MvpView {
 
     void showSpeaker(Speaker speaker);
